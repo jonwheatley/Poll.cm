9347bb9cf1074d733ec005a1e3efbef661129991
Jon Wheatley <jonwheatley@gmail.com>
2011-06-29 22:49:56 -0700

    kinda finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :footer
+  
+  def footer
+    @totalquestions = Question.all.count+6206
+    @totalvotes = Vote.all.count+54742
+  end
 end
