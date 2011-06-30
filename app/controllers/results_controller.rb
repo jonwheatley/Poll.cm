56c3d2171db29e19909fa9c7ea0b91f19aea7095
Jon Wheatley <jonwheatley@gmail.com>
2011-06-29 22:32:29 -0700

    fixed graph

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -6,6 +6,18 @@ class ResultsController < ApplicationController
     
     @total = Vote.where(:question_id => params[:id]).count
     @av = 100/@total
+    
+    @optionsgraph = ""
+    @votesgraph = ""
+    
+    @options.each do |option|
+      @optionsgraph = "#{@optionsgraph}|#{option.option[0..10]}..."
+      @votesgraph = "#{@votesgraph},#{option.votes.count}"
+    end
+    
+    @optionsgraph = @optionsgraph[1..-1]
+    @votesgraph = @votesgraph[1..-1]
+    
   end
   
 end
