1ff4f8ffd6b1689f66b7b7b0ddb68f1fc37c5bb9
Jon Wheatley <jonwheatley@gmail.com>
2011-06-30 00:05:43 -0700

    now there's only 1 vote per IP

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,7 +1,14 @@
 class OptionsController < ApplicationController
   
   def show
+    
     @question = Question.find(params[:id])
+    
+    @client_ip = request.remote_ip 
+    if !Vote.where(:question_id => params[:id].to_i, :ip => @client_ip).empty?
+      redirect_to(results_path(@question))
+    end
+    
     @options = Option.where(:question_id => params[:id])
     @votes = Vote.new
   end
