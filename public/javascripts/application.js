8e68ce6b441ad131093653d71defaf3c25c3769f
Jon Wheatley <jonwheatley@gmail.com>
2011-07-04 12:50:23 -0700

    made shit update in real time

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,3 +15,15 @@ function refreshpage () {
   $.getScript("/results.js?id=" + question_id)
   setTimeout(refreshpage, 10000);
 }
+
+$(function() {
+  if ($("#stats").length > 0) {
+    setTimeout(refreshpage, 10000);
+  }
+});
+
+function refreshpage () {
+  var question_id = $("#stats")
+  $.getScript("/stats.js")
+  setTimeout(refreshpage, 10000);
+}
